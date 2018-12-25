efb08a073ab989da395b9aef3fb00100db0e34a3
Niels Dingsbums <niels.dingsbums@gmx.de>
2018-12-25 11:33:19 +0100

    add comments

--- mod/mod.js
+++ mod/mod.js
@@ -1,14 +1,48 @@
-var ws = new WebSocket("ws://localhost:361");
+// mod.js  
 
-var data = null;
+var ws = new WebSocket("ws://localhost:361/");
 
-ws.onmessage = function() {
-	data = JSON.parse(event.data).data;
+var response = null;
+
+/*
+var exampleRequest = {type: "initCell", data: {name: "jesus"}};
+var exampleResponse = {type: "initCell", data: {id: 0, name: "jesus", pos: [100, 342], alive: true, kills: 0};
+
+The server responds with a message of the type / scheme seen in "exampleResponse".
+The type of this message is always the same as in the request.
+Data in the request *is not* proprietary.
+Data in the response on the other hand *is* proprietary (unless the request is invalid).
+*/
+
+/*
+Is getting called whenever the server sends any message / response.
+Sets the global variable "response".
+
+I think this is the spot for a callback ... idk?
+*/
+ws.onmessage = () => {
+	response = JSON.parse(event.data);
 }
 
-function wsCommand(type, paramData) {
-	ws.send(JSON.stringify({type: type, data: paramData}));
+/*
+example usage:
+wsRequest({type: "initCell", data: {name: "jesus"}});
 
-	console.log(data);
-	data = null;
+It returns the response of the server.
+*/
+function wsRequest(msg) {
+	ws.send(JSON.stringify(msg));
+
+	//wait until the response variable has changed, thus is not null (great english!!)
+
+	let data = response;
+
+	// reset response
+	response = null;
+
+	//return it
+	return data;
 }
+
+
+// jesus christ I have more comments than code. (exhales)
