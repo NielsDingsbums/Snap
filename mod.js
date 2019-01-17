2efe6fea845127499eace54d723fcbb8492a43b7
Niels Dingsbums <niels.dingsbums@gmx.de>
2019-01-17 21:47:25 +0100

    feat: ready

--- mod.js
+++ mod.js
@@ -2,32 +2,41 @@
 
 var ws = new WebSocket("ws://localhost:361/");
 
-var response = null;
+var world;
+window.onload = function () {
+	world = new WorldMorph(document.getElementById('world'));
+	world.worldCanvas.focus();
+	new IDE_Morph().openIn(world);
+	loop();
+	var stage = world.children[0].children.filter((item) => {return item instanceof StageMorph;})
+};
+function loop() {
+    requestAnimationFrame(loop);
+    //console.log("test")
+	ready = world.children[0].projectName == "project";
+	if (ready) {
+		wsRequest({type: "ready"})
+	}
+    world.doOneCycle();
+}
 
-/*
-var exampleRequest = {type: "initCell", data: {name: "jesus"}};
-var exampleResponse = {type: "initCell", data: {id: 0, name: "jesus", pos: [100, 342], alive: true, kills: 0};
 
-The server responds with a message of the type / scheme seen in "exampleResponse".
-The type of this message is always the same as in the request.
-Data in the request *is not* proprietary.
-Data in the response on the other hand *is* proprietary (unless the request is invalid).
-*/
+var state;
 
 ws.onmessage = () => {
-	callbacks[JSON.parse(event.data).type](event)
-	// delete itttttt (lit)
-	delete callbacks[JSON.parse(event.data).type]
-
+	let type = JSON.parse(event.data).type;
+	if (type == "state") {
+		state = JSON.parse(event.data).data;
+	} else if (type == "") {
+		// do nothing
+	} else {
+		callbacks[JSON.parse(event.data).type](event)
+		// delete itttttt
+		delete callbacks[JSON.parse(event.data).type]
+	}
 }
+
 var callbacks = {
-	// exampleStructure after wsRequest({type: "initCell", data: {name: "jesus"}}, (data) => {console.log(data)}) was called
-	
-	/*
-		initCell: (data) => {
-			console.log(data);
-		}
-	*/
 }
 
 /*
@@ -38,10 +47,85 @@ wsRequest("initCell",{type: "initCell", data: {name: "jesus"}},(data) => {
 
 It returns the response of the server.
 */
+
 function wsRequest(msg, cb) {
 	ws.send(JSON.stringify(msg));
 	callbacks[msg.type] = cb;
 }
+
+
+
+
+
+function updateEverything() {
+	let foodClones = stage.children.filter((item) => {return item instanceof SpriteMorph && item.name === "food"}).instances
+	let cellClones = stage.children.filter((item) => {return item instanceof SpriteMorph && item.name === "cell"}).instances
+	let currentPlayerMorph = stage.children.filter((item) => {return item instanceof SpriteMorph && item.name === "currentPlayer"})
+
+	if (foodClones.length === state.food.length) {
+		foodClone.forEach((morph) => {	
+			// position
+			let pos = toRel(currentPlayer.pos, item.pos);
+			clone.gotoXY(pos[0], pos[1]);
+
+			// set size (scale)
+			clone.setScale(10);
+		})
+	} else {
+		foodClones.forEach((item) => {
+			item.removeClone();
+		})
+		
+		state.food.forEach((item) => {
+			let clone = this.createClone();
+			
+			// position
+			let pos = toRel(currentPlayer.pos, item.pos);
+			clone.gotoXY(pos[0], pos[1]);
+
+			clone.setScale(10);
+
+			// set uid
+			clone.variables.vars.id.value = item.id;
+		})
+	}
+	
+	if (cellClones.length == state.cells.length) {
+		cellClones.forEach((item, index) => {
+			let cell = this[index];
+
+			// position
+			let pos = toRel(currentPlayer.pos, cell.pos);
+			clone.gotoXY(pos[0], pos[1]);
+
+			// set size (scale)
+			clone.setScale(cell.size);
+
+			// set uid
+			clone.variables.vars.id.value = cell.id;
+
+		}, this.state.cells);
+	} else {
+		cellClones.forEach((item) => {
+			item.removeClone();
+		})
+		
+		state.cells.forEach((item) => {
+			let clone = this.createClone();
+			
+			// position
+			let pos = toRel(currentPlayer.pos, item.pos);
+			clone.gotoXY(pos[0], pos[1]);
+
+			// set size (scale)
+			clone.setScal(item.size);
+
+			// set uid
+			clone.variables.vars.id.value = item.id;
+		})
+	}
+}
+
 
 function listify(jsonObject) {
   if (jsonObject instanceof Array) {
@@ -71,6 +155,5 @@ function toAbs(center, point) {
 
 // game vars
 
-var cells = [];
-var food = [];
-var currentPlayer = {};
+var gameover = false;
+var velo =[] ;
